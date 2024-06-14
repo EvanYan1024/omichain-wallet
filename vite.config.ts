4a8d931a42620ed1aeb12950e39968786e4fb68d
YanYuan <yan950414@gmail.com>
2024-06-14 16:30:56 +0800

    feat: buffer

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,28 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from "path"
-import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
-
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  define: { 'process.env': {} },
-  plugins: [react()],
+  plugins: [
+      react(),
+      nodePolyfills({
+        exclude: [],
+        globals: {
+          Buffer: true,
+          global: true,
+          process: true
+        },
+        protocolImports: true
+      })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   optimizeDeps: {
-    esbuildOptions: {
-      // Node.js global to browser globalThis
-      define: {
-        global: 'globalThis',
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true
-        })
-      ]
-    },
   },
   server: {
     port: 8000
